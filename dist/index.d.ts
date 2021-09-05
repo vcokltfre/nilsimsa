b6c90bee7c3de68ef256e87387175c32a88c8102
vcokltfre <vcokltfre@gmail.com>
2021-09-05 23:35:08 +0100

    fix: use correct types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,13 +4,13 @@
  * @param input - The buffer to compute a digest for
  * @returns Byte digest
  */
-export declare function digest(input: Buffer): number[];
+export declare function digest(input: Uint8Array): number[];
 /**
  * Compute a nilsimsa digest hex for the provided buffer
  * @param input - The buffer to compute a digest for
  * @returns Hexstring digest
  */
-export declare function hexDigest(input: Buffer): string;
+export declare function hexDigest(input: Uint8Array): string;
 /**
  * Compare two nilsimsa digest byte arrays
  * @param a - The first digest to compare
